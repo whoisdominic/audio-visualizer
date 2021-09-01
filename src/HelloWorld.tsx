e405c26b21fe9d651afaaf2b1f7782d933a235eb
Dominic <me@dominiccobb.dev>
2021-09-01 07:12:28 -0700

    Video basics done

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,39 +1,102 @@
-import {interpolate, Sequence, useCurrentFrame, useVideoConfig} from 'remotion';
-import {Logo} from './HelloWorld/Logo';
-import {Subtitle} from './HelloWorld/Subtitle';
-import {Title} from './HelloWorld/Title';
+import {Audio, useCurrentFrame, useVideoConfig, Img} from 'remotion';
+import {useAudioData, visualizeAudio} from '@remotion/media-utils';
+import music from './makeAVideoWithReact.m4a';
+import profileImage from './3AB724E1-110A-4959-A4AF-E527F428BDE3_1_102_o.jpeg';
 
-export const HelloWorld: React.FC<{
-	titleText: string;
-	titleColor: string;
-}> = ({titleText, titleColor}) => {
+export const HelloWorld: React.FC = () => {
 	const frame = useCurrentFrame();
-	const videoConfig = useVideoConfig();
-
-	const opacity = interpolate(
+	const {width, height, fps} = useVideoConfig();
+	const audioData = useAudioData(music);
+	if (!audioData) {
+		return null;
+	}
+	const visualization = visualizeAudio({
+		fps,
 		frame,
-		[videoConfig.durationInFrames - 25, videoConfig.durationInFrames - 15],
-		[1, 0],
-		{
-			extrapolateLeft: 'clamp',
-			extrapolateRight: 'clamp',
-		}
-	);
-	const transitionStart = 25;
+		audioData,
+		numberOfSamples: 32,
+	});
 
 	return (
-		<div style={{flex: 1, backgroundColor: 'white'}}>
-			<div style={{opacity}}>
-				<Sequence from={0} durationInFrames={videoConfig.durationInFrames}>
-					<Logo transitionStart={transitionStart} />
-				</Sequence>
-				<Sequence from={transitionStart + 10} durationInFrames={Infinity}>
-					<Title titleText={titleText} titleColor={titleColor} />
-				</Sequence>
-				<Sequence from={transitionStart + 50} durationInFrames={Infinity}>
-					<Subtitle />
-				</Sequence>
-			</div>
+		<div
+			style={{
+				display: 'flex',
+				backgroundColor: '#000',
+				justifyContent: 'center',
+				alignItems: 'center',
+				width: 1080,
+				height: 1080,
+			}}
+		>
+			<Audio src={music} />
+			<Img
+				src={profileImage}
+				style={{
+					position: 'absolute',
+					width: 150,
+					height: 150,
+					borderRadius: 500,
+					zIndex: 3,
+				}}
+			/>
+			{visualization.map((item: number, index: number) => {
+				return (
+					<>
+						<div
+							style={{
+								width: 140 + item * 933.33,
+								height: 1.5,
+								backgroundColor: 'rgba(92, 92, 92, .833)',
+								zIndex: 0,
+								transform: `rotate(${(360 / visualization.length) * index}deg)`,
+								position: 'absolute',
+							}}
+						/>
+						<div
+							style={{
+								width: 140 + item * 933.33,
+								height: 1.5,
+								backgroundColor: 'rgba(92, 92, 92, .833)',
+								zIndex: 0,
+								transform: `rotate(${
+									15 + (360 / visualization.length) * index
+								}deg)`,
+								position: 'absolute',
+							}}
+						/>
+					</>
+				);
+			})}
+			{visualization.map((item: number, index: number) => {
+				return (
+					<>
+						<div
+							style={{
+								width: 140 + item * 933.33,
+								height: 1.5,
+								backgroundColor: 'rgba(92, 92, 92, .833)',
+								zIndex: 0,
+								transform: `rotate(${
+									-(360 / visualization.length) * index
+								}deg)`,
+								position: 'absolute',
+							}}
+						/>
+						<div
+							style={{
+								width: 140 + item * 933.33,
+								height: 1.5,
+								backgroundColor: 'rgba(92, 92, 92, .833)',
+								zIndex: 0,
+								transform: `rotate(${
+									-(360 / visualization.length) * index + 15
+								}deg)`,
+								position: 'absolute',
+							}}
+						/>
+					</>
+				);
+			})}
 		</div>
 	);
 };
